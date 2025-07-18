3072734fe5d1f3f1a9fa8e5442627cf8a9629904
Maham Tahir <mahamst31@gmail.com>
2025-07-18 15:46:09 +0500

    added on 18 July, 2025

---
+++ README.md
@@ -0,0 +1,3 @@
+<h1> Practical React Essentials</h1>
+
+<p>This is my practice app for the react course</p>
---
+++ src/App.tsx
@@ -0,0 +1,15 @@
+import './App.css'
+
+function App() {
+
+  return (
+    <>
+      <h1 className='mb-4'>
+        Hi there! Welcome...
+      </h1>
+      <p>Maham Tahir</p>
+    </>
+  )
+}
+
+export default App
